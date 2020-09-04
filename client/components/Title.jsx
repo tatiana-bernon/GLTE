a3659d0591b4eec52ad98b5c809ca52f49a6dcc5
Gideon Cooper <66655969+gideon-cooper@users.noreply.github.com>
2020-09-04 14:35:05 +1200

    some styling

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 export default class GameOver extends Component {
   render () {
     return (
-      <div>
-        <h1>Congratulations, Pohutukawa!</h1>
-        <h2>Let's Have Some Fun</h2>
+      <div className="rows is-vcentered">
+        <h1 className="row">Congratulations, Pohutukawa!</h1>
+        <h2 className="row">Let's Have Some Fun</h2>
 
-        <img
-          style={{ height: '300px', width: '250px' }}
+        <img className="row"
+          style={{ height: '300px', width: '400px' }}
           // src={'https://i.pinimg.com/originals/10/80/a4/1080a4bd1a33cec92019fab5efb3995d.png'}
           src={'https://media.giphy.com/media/3EfgWHj0YIDrW/giphy.gif'}
         />
         <br></br>
-        <Link to={{ pathname: '/Card' }} ><button className="button is-rounded">Play Now!</button></Link>
+        <Link to={{ pathname: '/Card' }} ><button className="button is-rounded ">Play Now!</button></Link>
       </div>
     )
   }
